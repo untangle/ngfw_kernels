90c15d4ba19602f9a67d43c7955a1dafb0eabdeb
John Irwin <jdi@untangle.com>
2008-03-11 16:41:53 +0000

    24 kernel moved from /work/pkgs to /kernels
    git-svn-id: https://untangle.svn.beanstalkapp.com/ngfw/kernels@15140 8ebf8b4e-06f3-0310-961f-89f74618be54
    
    Former-commit-id: 71dcc2fafeaa00d4970cde6a59f6c2b973073722

---
+++ untangle-linux-2.6.24/common.mk
@@ -0,0 +1,136 @@
+# Our version number.  Currently must be manually updated.  You've been warned.
+OURVERSION=untangle4
+
+# Upstream version numbers
+KVER=2.6.24
+KDEBVER=10
+
+KUPVER=${KDEBVER}.16
+PACKPFX=linux_${KVER}
+WORKDIR=linux-${KVER}
+KDSC=${PACKPFX}-${KUPVER}.dsc
+KOURPATCH=untangle-linux.diff.gz
+
+UMUPVER=${KDEBVER}.14
+UMPFX=linux-ubuntu-modules-${KVER}_${KVER}
+UMWORKDIR=linux-ubuntu-modules-${KVER}-${KVER}
+UMDSC=${UMPFX}-${UMUPVER}.dsc
+UMOURPATCH=untangle-linux-ubuntu-modules.diff.gz
+
+RMUPVER=${KDEBVER}.27
+RMVER=9
+RMPFX=linux-restricted-modules-${KVER}_${KVER}.${RMVER}
+RMWORKDIR=linux-restricted-modules-${KVER}-${KVER}.${RMVER}
+RMDSC=${RMPFX}-${RMUPVER}.dsc
+RMOURPATCH=untangle-linux-restricted-modules.diff.gz
+
+METAUPVER=${KDEBVER}.8
+METAPFX=linux-meta_${KVER}
+METAWORKDIR=linux-meta-${KVER}.${METAUPVER}
+METADSC=${METAPFX}.${METAUPVER}.dsc
+METAOURPATCH=untangle-linux-meta.diff.gz
+
+EXTRAPATCHES ?=
+EXTRADCHARGS ?=
+
+DOPTIONS=DEB_BUILD_OPTIONS="parallel=${NPROCEXP}" AUTOBUILD=1 KVERS=${KVER}-${KDEBVER}-untangle
+VDOPTIONS=DEB_BUILD_OPTIONS="parallel=${NPROCEXP}" AUTOBUILD=1 KVERS=${KVER}-${KDEBVER}-virtual-untangle
+
+
+NPROCEXP:=$(shell grep processor /proc/cpuinfo|wc -l)
+TEMPDIR:=$(shell mktemp)
+
+
+all:	clean pkgs legitify
+
+pkgs::	kpkg umpkg
+
+deps:	force
+	sudo apt-get install ${BUILDDEPS} || echo "unable to run sudo"
+
+kpkg:	${WORKDIR} force
+	cd ${WORKDIR}; ${DOPTIONS} fakeroot debian/rules clean custom-binary-untangle
+	cd ${WORKDIR}; ${VDOPTIONS} fakeroot debian/rules custom-binary-virtual-untangle
+	cd ${WORKDIR}; ${DOPTIONS} fakeroot debian/rules binary-indep binary-arch-headers
+
+umpkg:	${UMWORKDIR} force
+# Need something better than this.
+	sudo dpkg -i linux-headers-${KVER}-${KDEBVER}_${KVER}-${KUPVER}${OURVERSION}_all.deb linux-headers-${KVER}-${KDEBVER}-untangle_${KVER}-${KUPVER}${OURVERSION}_i386.deb linux-headers-${KVER}-${KDEBVER}-virtual-untangle_${KVER}-${KUPVER}${OURVERSION}_i386.deb
+	sudo rm -f /usr/src/linux-headers-untangle;sudo ln -s /usr/src/linux-headers-${KVER}-${KDEBVER}-untangle /usr/src/linux-headers-untangle
+	sudo rm -f /usr/src/linux-headers-virtual-untangle;sudo ln -s /usr/src/linux-headers-${KVER}-${KDEBVER}-virtual-untangle /usr/src/linux-headers-virtual-untangle
+	cd ${UMWORKDIR}; ${DOPTIONS} fakeroot debian/rules clean binary-debs arch=i386 flavours="untangle"
+	cd ${UMWORKDIR}; ${VDOPTIONS} fakeroot debian/rules clean binary-debs arch=i386 flavours="virtual-untangle"
+
+rmpkg:	${RMWORKDIR} force
+# Need something better than this.
+	sudo dpkg -i linux-headers-${KVER}-${KDEBVER}_${KVER}-${KUPVER}${OURVERSION}_all.deb linux-headers-${KVER}-${KDEBVER}-untangle_${KVER}-${KUPVER}${OURVERSION}_i386.deb
+	sudo rm -f /usr/src/linux-headers-untangle;sudo ln -s /usr/src/linux-headers-${KVER}-${KDEBVER}-untangle /usr/src/linux-headers-untangle
+	cd ${RMWORKDIR}; ${DOPTIONS} fakeroot debian/rules clean binary-debs arch=i386 flavours="${KVER}-${KDEBVER}-untangle" ati_flavours="${KVER}-${KDEBVER}-untangle" nv_flavours="${KVER}-${KDEBVER}-untangle"
+	cd ${RMWORKDIR}; ${DOPTIONS} fakeroot debian/rules binary-indep arch=i386 flavours="${KVER}-${KDEBVER}-untangle" ati_flavours="${KVER}-${KDEBVER}-untangle" nv_flavours="${KVER}-${KDEBVER}-untangle"
+
+metapkg:	${METAWORKDIR} force
+	cd ${METAWORKDIR}; ${DOPTIONS} fakeroot debian/rules clean binary arch=i386 flavours="untangle"
+	cd ${METAWORKDIR}; ${VDOPTIONS} fakeroot debian/rules clean binary arch=i386 flavours="virtual-untangle"
+
+
+# This combines our generic kernel patches with the specific Ubuntu kernel
+# patches into one patch to rule them all.
+UPATCHSETLOC=${WORKDIR}/debian/binary-custom.d/untangle/patchset
+UVPATCHSETLOC=${WORKDIR}/debian/binary-custom.d/virtual-untangle/patchset
+${KOURPATCH}: untangle-linux.basediff patches/0*
+	rm -rf ${TEMPDIR}
+	mkdir -p ${TEMPDIR}/${UPATCHSETLOC}
+	mkdir -p ${TEMPDIR}/old/${UPATCHSETLOC}
+	cp patches/0* ${TEMPDIR}/${UPATCHSETLOC}
+	if [ -n "${EXTRAPATCHES}" ]; then cp "${EXTRAPATCHES}" ${TEMPDIR}/${UPATCHSETLOC}; fi
+	-cd ${TEMPDIR};diff -Ncr old/${UPATCHSETLOC} ${UPATCHSETLOC} > ${TEMPDIR}/all.diff
+	mkdir -p ${TEMPDIR}/${UVPATCHSETLOC}
+	mkdir -p ${TEMPDIR}/old/${UVPATCHSETLOC}
+	cp patches/0* ${TEMPDIR}/${UVPATCHSETLOC}
+	if [ -n "${EXTRAPATCHES}" ]; then cp "${EXTRAPATCHES}" ${TEMPDIR}/${UVPATCHSETLOC}; fi
+	-cd ${TEMPDIR};diff -Ncr old/${UVPATCHSETLOC} ${UVPATCHSETLOC} >> ${TEMPDIR}/all.diff
+	cat untangle-linux.basediff >> ${TEMPDIR}/all.diff
+	if [ -f untangle-linux.${REPOSITORY}diff ]; then cat untangle-linux.${REPOSITORY}diff >> ${TEMPDIR}/all.diff; fi
+	gzip -c ${TEMPDIR}/all.diff > ${KOURPATCH}
+
+${WORKDIR}:	${KDSC} ${KOURPATCH}
+	rm -rf ${WORKDIR}
+	dpkg-source -x ${KDSC}
+	cd ${WORKDIR};gunzip -c ../${KOURPATCH} | patch -p1
+# Clean out control.stub so that it always gets generated.
+	rm ${WORKDIR}/debian/control.stub
+	cd ${WORKDIR}; DEBEMAIL="jdi@untangle.com" dch ${EXTRADCHARGS} -p -v ${KVER}-${KUPVER}${OURVERSION} -D thunderbird "kernel build"
+
+${UMWORKDIR}:	${UMDSC} ${UMOURPATCH}
+	rm -rf ${UMWORKDIR}
+	dpkg-source -x ${UMDSC}
+	cd ${UMWORKDIR};gunzip -c ../${UMOURPATCH} | patch -p1
+	cd ${UMWORKDIR}; DEBEMAIL="jdi@untangle.com" dch ${EXTRADCHARGS} -p -v ${KVER}-${UMUPVER}${OURVERSION} -D thunderbird "kernel build"
+
+${RMWORKDIR}:	${RMDSC} ${RMOURPATCH}
+	rm -rf ${RMWORKDIR}
+	dpkg-source -x ${RMDSC}
+	cd ${RMWORKDIR};gunzip -c ../${RMOURPATCH} | patch -p1
+	cd ${RMWORKDIR}; DEBEMAIL="jdi@untangle.com" dch ${EXTRADCHARGS} -p -v ${KVER}.${RMVER}-${RMUPVER}${OURVERSION} -D thunderbird "kernel build"
+
+${METAWORKDIR}:	${METADSC} ${METAOURPATCH}
+	rm -rf ${METAWORKDIR}
+	dpkg-source -x ${METADSC}
+	cd ${METAWORKDIR};gunzip -c ../${METAOURPATCH} | patch -p1
+	cd ${METAWORKDIR}; DEBEMAIL="jdi@untangle.com" dch ${EXTRADCHARGS} -p -v ${KVER}.${METAUPVER}${OURVERSION} -D thunderbird "kernel build"
+
+release: force
+	echo "make -f HADESHOME/pkgtools/Makefile release-deb REPOSITORY=${REPOSITORY} DISTRIBUTION=thunderbird"
+
+
+clean::
+	rm -f ${KOURPATCH}
+	rm -rf ${WORKDIR}
+	rm -rf ${UMWORKDIR}
+	rm -rf ${RMWORKDIR}
+	rm -rf ${METAWORKDIR}
+	rm -rf notlegit
+	rm -f *.deb modules/*.deb
+	rm -f *.udeb modules/*.udeb
+
+force:
---
+++ untangle-linux-2.6.24/linux-restricted-modules-2.6.24_2.6.24.9-10.27.dsc
@@ -0,0 +1,23 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 1.0
+Source: linux-restricted-modules-2.6.24
+Binary: linux-restricted-modules-common, linux-restricted-modules-2.6.24-10-386, nvidia-glx, nvidia-glx-dev, nvidia-kernel-source, nvidia-glx-new, nvidia-glx-new-dev, nvidia-new-kernel-source, nvidia-glx-legacy, nvidia-glx-legacy-dev, nvidia-legacy-kernel-source, fglrx-kernel-source, xorg-driver-fglrx, xorg-driver-fglrx-dev, fglrx-control, fglrx-amdcccle, linux-restricted-modules-2.6.24-10-generic, linux-restricted-modules-2.6.24-10-rt, linux-restricted-modules-2.6.24-10-server, linux-restricted-modules-2.6.24-10-powerpc, linux-restricted-modules-2.6.24-10-powerpc-smp, linux-restricted-modules-2.6.24-10-powerpc64-smp, linux-restricted-modules-2.6.24-10-itanium, linux-restricted-modules-2.6.24-10-mckinley, linux-restricted-modules-2.6.24-10-hppa32, linux-restricted-modules-2.6.24-10-hppa64, linux-restricted-modules-2.6.24-10-lpia, linux-restricted-modules-2.6.24-10-lpiacompat, linux-restricted-modules-2.6.24-10-sparc64, linux-restricted-modules-2.6.24-10-sparc64-smp, linux-restricted-modules-2.6.24-10-xen
+Architecture: any
+Version: 2.6.24.9-10.27
+Maintainer: Ubuntu Kernel Team <kernel-team@lists.ubuntu.com>
+Uploaders: Adam Conrad <adconrad@0c3.net>, Ben Collins <ben.collins@ubuntu.com>
+Standards-Version: 3.6.1
+Build-Depends: bzip2, cpio, debhelper (>= 4.0.0), kernel-wedge (>= 2.05ubuntu2), libc6-i386 [amd64], libgtk2.0-dev [i386 amd64], linux-headers-2.6.24-10-386 [i386], linux-headers-2.6.24-10-generic [amd64 i386], linux-headers-2.6.24-10-hppa32 [hppa], linux-headers-2.6.24-10-hppa64 [hppa], linux-headers-2.6.24-10-itanium [ia64], linux-headers-2.6.24-10-lpia [lpia], linux-headers-2.6.24-10-lpiacompat [lpia], linux-headers-2.6.24-10-mckinley [ia64], linux-headers-2.6.24-10-powerpc [powerpc], linux-headers-2.6.24-10-powerpc-smp [powerpc], linux-headers-2.6.24-10-powerpc64-smp [powerpc], linux-headers-2.6.24-10-rt [i386 amd64], linux-headers-2.6.24-10-server [amd64 i386], linux-headers-2.6.24-10-sparc64 [sparc], linux-headers-2.6.24-10-sparc64-smp [sparc], linux-headers-2.6.24-10-xen [amd64 i386], module-init-tools, rpm, sed (>> 3.0), sharutils
+Files: 
+ 75622cb6f30b987d41ccd60ebae898f4 149556088 linux-restricted-modules-2.6.24_2.6.24.9.orig.tar.gz
+ 1585d59d8db9e376e41d4ea44c10c246 109678 linux-restricted-modules-2.6.24_2.6.24.9-10.27.diff.gz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.6 (GNU/Linux)
+
+iD8DBQFHxIpSCf/ckHZoKjcRAj4qAJ0WD5Id0U+JohnpTucxbY/REOMNOACfdDGK
+r8LAE4i3ZLXCT9UH5EzOdrM=
+=T/t1
+-----END PGP SIGNATURE-----
